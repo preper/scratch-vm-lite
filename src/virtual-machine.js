58aed0a58df637337da8e0716199fe272e232900
preper <qpreper@hotmail.com>
2021-03-02 14:24:39 +0800

    feat: 移除对sb1文件的支持

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -317,29 +317,7 @@ class VirtualMachine extends EventEmitter {
                 if (error) return reject(error);
                 resolve(res);
             });
-        })
-            .catch(error => {
-                const {SB1File, ValidationError} = require('scratch-sb1-converter');
-
-                try {
-                    const sb1 = new SB1File(input);
-                    const json = sb1.json;
-                    json.projectVersion = 2;
-                    return Promise.resolve([json, sb1.zip]);
-                } catch (sb1Error) {
-                    if (sb1Error instanceof ValidationError) {
-                        // The input does not validate as a Scratch 1 file.
-                    } else {
-                        // The project appears to be a Scratch 1 file but it
-                        // could not be successfully translated into a Scratch 2
-                        // project.
-                        return Promise.reject(sb1Error);
-                    }
-                }
-                // Throw original error since the input does not appear to be
-                // an SB1File.
-                return Promise.reject(error);
-            });
+        });
 
         return validationPromise
             .then(validatedInput => this.deserializeProject(validatedInput[0], validatedInput[1]))
